f221c8da7693d8f6ba58bfef2311af319f48f7f5
flamypow <wytchen90s@gmail.com>
2024-09-13 09:06:36 -0600

    Improved the Template, introduced GameLogic GameObject

--- Assets/Code/Scripts/GameLogic.cs
+++ Assets/Code/Scripts/GameLogic.cs
@@ -2,19 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseActScene : MonoBehaviour
+public enum GameMode
+{
+    PRESSANDHOLD,
+    TIMEDCOOKING,
+    STACKER,
+    RHYTHM
+}
+
+public class GameLogic : Singleton<GameLogic>
 {
     [SerializeField] private int leeway; //leeway for this level
-    
+    [SerializeField] private GameMode currentSceneGameMode;
     //Structure basic game logic here. 
 
     public void RecordScore()
-    { 
+    {
         //tells ScoreManager to add a score. 
     }
 
     public void SetTargets()
-    { 
+    {
         //tells ScoreManager to set targets
     }
 }
--- Assets/Code/Scripts/GameManager.cs
+++ Assets/Code/Scripts/GameManager.cs
@@ -7,6 +7,6 @@ public class GameManager : Singleton<GameManager>
     //this is just a hub for communicating for now. 
     public int gameplayPlayerPercentValue;
 
-    //finds the Act#Scene# and command it. 
-    //if need to grab and pass UIButtonInfo to Act#Scene#. 
+    //get the Gamelogic and commands it
+    //if need to grab and pass UIButtonInfo to GameLogic 
 }
