2ddcbeaef8d441de0feca29d25454e9fe3feec6d
flamypow <wytchen90s@gmail.com>
2024-09-13 13:49:14 -0600

    GameLogic_Rhythm Implementation 09/13/2024

--- Assets/Code/Scripts/GameLogic.cs
+++ Assets/Code/Scripts/GameLogic.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,11 +11,20 @@ public enum GameMode
     RHYTHM
 }
 
-public class GameLogic : Singleton<GameLogic>
+public class GameLogic : Singleton<GameLogic>, IButtonListener
 {
     [SerializeField] private int leeway; //leeway for this level
     [SerializeField] private GameMode currentSceneGameMode;
     //Structure basic game logic here. 
+
+    [SerializeField] private float gameSpeed = 15f;
+
+    float startTime;
+    float currentTime;
+
+    int currentTimedPercent;
+    int currentPlayerPercent;
+    bool countingUpwards;
 
     public void RecordScore()
     {
@@ -24,5 +34,56 @@ public class GameLogic : Singleton<GameLogic>
     public void SetTargets()
     {
         //tells ScoreManager to set targets
+    }
+
+    public void Start()
+    {
+        FindObjectOfType<PlayerInputs>().RegisterListener(this);
+        startTime = Time.time;
+        countingUpwards = true;
+        if (currentSceneGameMode == GameMode.RHYTHM)
+        {
+            currentTimedPercent = 0;
+        }
+    }
+
+    public void Update()
+    {
+        currentTime = Time.time;
+
+        if (currentSceneGameMode == GameMode.RHYTHM)
+        {
+            countingUpwards = ((int)(((currentTime - startTime) * gameSpeed)/100) %2 )== 0;
+            currentTimedPercent = (int)((currentTime - startTime)* gameSpeed)%100;
+            //Debug.Log(countingUpwards);
+            //Debug.Log((countingUpwards) ? currentTimedPercent : 100-currentTimedPercent);
+
+
+
+        }
+    }
+
+    public void ButtonHeld(ButtonInfo heldInfo)
+    {
+        //Debug.Log("button Held");
+    }
+
+    public void ButtonPressed(ButtonInfo pressedInfo)
+    {
+        if (currentSceneGameMode == GameMode.RHYTHM)
+        {
+            //record value here and send both this and currentTimedPercent to scoremanager
+            Debug.Log((countingUpwards) ? currentTimedPercent : 100 - currentTimedPercent);
+        }
+    }
+
+    public void ButtonReleased(ButtonInfo releasedInfo)
+    {
+        Debug.Log("buttonReleased");
+        if (currentSceneGameMode == GameMode.RHYTHM)
+        {
+            //record value here and send both this and currentTimedPercent to scoremanager
+            Debug.Log((countingUpwards) ? currentTimedPercent : 100 - currentTimedPercent);
+        }
     }
 }
