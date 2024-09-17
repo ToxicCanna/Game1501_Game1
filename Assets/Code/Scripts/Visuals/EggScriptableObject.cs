b4118231319ac8d9d64322431d26a2c1661f6537
flamypow <wytchen90s@gmail.com>
2024-09-16 21:35:23 -0600

    Act2 Scene3 progress

--- Assets/Code/Scripts/Visuals/EggScriptableObject.cs
+++ Assets/Code/Scripts/Visuals/EggScriptableObject.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/EggScriptableObject", order = 1)]
+[CreateAssetMenu(fileName = "DataForEgg", menuName = "ScriptableObjects/EggScriptableObject", order = 1)]
 public class EggScriptableObject : ScriptableObject
 {
     [SerializeField] public Sprite[] eggBody;
