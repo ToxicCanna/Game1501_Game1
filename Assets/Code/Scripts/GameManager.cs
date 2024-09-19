57de7c63b35f152be3a60e819a03ee187128d5e1
ToxicCanna <freedom22140@gmail.com>
2024-09-19 00:02:02 -0600

    Scene Change script implementation
    integrated scene change script logic into game manager, cleaned up script logic in both game manager, and LevelChange script. script is for menu, game manager is for scene.
    
    Also implemented scene change for main menu into recipe select, and recipe select into starting scene for respective recipes

--- Assets/Code/Scripts/GameManager.cs
+++ Assets/Code/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : Singleton<GameManager>
 {
@@ -29,4 +30,22 @@ public class GameManager : Singleton<GameManager>
         gameUI.RecieveProgress(currentPercent, currentTarget, leeway);
     }
 
+    //When calling this function use the scene index in build settings to call specific scenes, EX. ChangeLevel(0) for main menu
+    public void ChangeLevel(int buildIndex)
+    {
+        Debug.Log("Changing Scene to " + buildIndex);
+        SceneManager.LoadScene(buildIndex, LoadSceneMode.Single);
+    }
+
+    //call if game is to quit, in editor this will close playmode, in a build it will close game
+    public void QuitGame()
+    {
+        Debug.Log("GameManager is quitting the game");
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; // Stop play mode in the Editor
+#else
+        Application.Quit();
+#endif
+    }
 }
--- Assets/Code/Scripts/Utilities/LevelChange.cs
+++ Assets/Code/Scripts/Utilities/LevelChange.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+//this levelchanger is for the menu scenes, as including the interconnected scripts for the menu is largely unneeded
+
 public class LevelChange : MonoBehaviour
 {
-    [SerializeField] int sceneBuildIndex;
-
-    public void SceneSwitch()
+    //When calling this function use the scene index in build settings to call specific scenes, EX. ChangeLevel(0) for main menu
+    public void SceneSwitch(int buildIndex)
     {
         print("Changing Scene");
-        print("Switching to " + sceneBuildIndex);
-        SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+        SceneManager.LoadScene(buildIndex, LoadSceneMode.Single);
+    }
+
+
+    //call if game is to quit, in editor this will close playmode, in a build it will close game
+    public void QuitGame()
+    {
+        Debug.Log("GameManager is quitting the game");
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; // Stop play mode in the Editor
+#else
+        Application.Quit();
+#endif
     }
 }
