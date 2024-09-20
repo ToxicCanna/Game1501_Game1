f86cf50e89b5eb3b42f567474835cc5d4e377ea4
Jahnavi Patel <j.patel717@mybvc.ca>
2024-09-20 14:20:05 +0530

    UI

---
+++ Assets/Code/Scripts/AudioManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("--------Audio Scorce---------")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("--------Audio Scorce---------")]
+    public AudioClip background;
+    public AudioClip cracking;
+    public AudioClip butter;
+    public AudioClip cake;
+}
